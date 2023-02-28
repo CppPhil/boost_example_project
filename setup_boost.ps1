87c4185501adbb9452100a0e74409e2bd4e2b73a
Phillip Schackier <CppPhil@users.noreply.github.com>
2023-02-28 20:33:12 +0100

    It does build Boost

---
+++ setup_boost.ps1
@@ -0,0 +1,45 @@
+# Update the version as needed.
+$boost_version = '1.81.0'
+
+$drive = 'C:'
+$directory_name = 'boost_' + $boost_version.Replace('.', '_')
+$archive_name = $directory_name + '.zip'
+$boost_root_path = "$drive\boost"
+$boost_directory_path = "$boost_root_path\$directory_name"
+$boost_archive_path = "$boost_root_path\$archive_name"
+$download_url = "https://boostorg.jfrog.io/artifactory/main/release/$boost_version/source/$archive_name"
+$bootstrap_script_path = "$boost_directory_path\bootstrap.bat"
+
+if (-Not (Test-Path -Path $boost_directory_path)) {
+  Write-Output "$boost_directory_path does not exist. Creating ..."
+  mkdir -p $boost_directory_path
+  Write-Output "Created $boost_directory_path."
+
+  Write-Output "Downloading $archive_name from $download_url, saving to $boost_archive_path ..."
+  Invoke-WebRequest $download_url -OutFile $boost_archive_path
+  Write-Output "Finished downloading $archive_name from $download_url to $boost_archive_path."
+
+  Write-Output "Extracting $boost_archive_path to $boost_root_path"
+  Expand-Archive $boost_archive_path -DestinationPath $boost_root_path
+  Write-Output "Extracted $boost_archive_path to $boost_root_path"
+
+  Write-Output "Deleting $boost_archive_path ..."
+  Remove-Item -Path $boost_archive_path -Force
+  Write-Output "Deleted $boost_archive_path."
+
+  Push-Location $boost_directory_path
+
+  Write-Output "Starting bootstrapping process ..."
+  Start-Process -FilePath $bootstrap_script_path -Wait -NoNewWindow -PassThru
+  Write-Output "Finished bootstrapping."
+
+  Write-Output "Starting build process for Boost $boost_version ..."
+  Start-Process -FilePath .\b2 -Wait -NoNewWindow -PassThru
+  Write-Output "Built Boost $boost_version."
+
+  Pop-Location
+  exit 0
+}
+
+Write-Output "$boost_directory_path already exists. Assuming it contains a valid installation of Boost $boost_version."
+exit 0
