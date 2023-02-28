b12520c7c5d904837bc8bb381d92a0fcafe8e533
Phillip Schackier <CppPhil@users.noreply.github.com>
2023-02-28 21:11:44 +0100

    Update, still won't work

--- build.ps1
+++ build.ps1
@@ -6,6 +6,7 @@ $directory_name = 'boost_' + $boost_version.Replace('.', '_')
 $archive_name = $directory_name + '.zip'
 $boost_root_path = "$drive\boost"
 $boost_directory_path = "$boost_root_path\$directory_name"
+$boost_library_path = "$boost_directory_path\stage\lib"
 $boost_archive_path = "$boost_root_path\$archive_name"
 $download_url = "https://boostorg.jfrog.io/artifactory/main/release/$boost_version/source/$archive_name"
 $bootstrap_script_path = "$boost_directory_path\bootstrap.bat"
@@ -51,7 +52,7 @@ if (-Not (Test-Path -Path $build_dir)) {
 $cpu_count = (Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors
 
 Set-Location $build_dir
-cmake -DCMAKE_BUILD_TYPE=Debug -DBOOST_VERSION_TO_USE="$boost_version" -DBOOST_ROOT="$boost_root_path" ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DBOOST_VERSION_TO_USE="$boost_version" -DBOOST_ROOT="$boost_root_path" -DBOOST_INCLUDEDIR="$boost_directory_path" -DBOOST_LIBRARYDIR="$boost_library_path" ..
 cmake --build . --config Debug --parallel $cpu_count
 
 if (-Not ($LASTEXITCODE -eq "0")) {
@@ -60,7 +61,7 @@ if (-Not ($LASTEXITCODE -eq "0")) {
   exit 1
 }
 
-cmake -DCMAKE_BUILD_TYPE=Release -DBOOST_VERSION_TO_USE="$boost_version" -DBOOST_ROOT="$boost_root_path" ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBOOST_VERSION_TO_USE="$boost_version" -DBOOST_ROOT="$boost_root_path" -DBOOST_INCLUDEDIR="$boost_directory_path" -DBOOST_LIBRARYDIR="$boost_library_path" ..
 cmake --build . --config Debug --parallel $cpu_count
 
 if (-Not ($LASTEXITCODE -eq "0")) {
