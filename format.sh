3edee09626f2fb377f5306da1800e7fee02ce803
Phillip Schackier <CppPhil@users.noreply.github.com>
2025-09-10 20:44:01 +0200

    Migrate to vcpkg

--- format.sh
+++ format.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-### This script is used to run clang-format on the source files.
-
 # Directory containing this bash script.
 readonly DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-format() {
-  find "$1" -type f -name '*.cpp' -o -name '*.hpp' | xargs clang-format -i
-}
-
-format "$DIR/src"
+find "$DIR/src" -type f \( -name "*.cpp" -o -name "*.hpp" -o -name "*.h" \) -exec clang-format -i {} +
+find "$DIR" -type f \( -name "CMakeLists.txt" \) -not \( -path "*external*" -o -path "*build*" \) -exec cmake-format -i {} +
 exit 0
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,25 @@
-#include <boost/regex.hpp>
 #include <iostream>
 #include <sstream>
 #include <string>
 
+#include <boost/regex.hpp>
+
 int main()
 {
-  std::istringstream iss(R"(To: George Shmidlap
+  std::istringstream inputStringStream{R"(To: George Shmidlap
 From: Rita Marlowe
 Subject: Will Success Spoil Rock Hunter?
 ---
-See subject.)");
-  std::string        line;
-  boost::regex       pat("^Subject: (Re: |Aw: )*(.*)");
+See subject.)"};
+  std::string        line{};
+  boost::regex       pattern{"^Subject: (Re: |Aw: )*(.*)"};
 
-  while (iss) {
-    std::getline(iss, line);
-    boost::smatch matches;
-    if (boost::regex_match(line, matches, pat))
-      std::cout << matches[2] << std::endl;
+  while (inputStringStream) {
+    std::getline(inputStringStream, line);
+    boost::smatch matches{};
+
+    if (boost::regex_match(line, matches, pattern)) {
+      std::cout << matches[2] << '\n';
+    }
   }
 }
